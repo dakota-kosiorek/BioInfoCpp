1c1c5d35028063b5abbe79eede65d415955b71ed
Dakota Kosiorek <84210378+dakota-kosiorek@users.noreply.github.com>
2024-01-04 23:18:28 -0500

    TotalPermutations
    Created TotalPermutations class that gets all possible permutations of a positive integer array of length n {1, 2, ..., n).

--- include/biomath.hpp
+++ include/biomath.hpp
@@ -1,10 +1,22 @@
 #ifndef BIOMATH_HPP
 #define BIOMATH_HPP 1
 
+#include <vector>
+#include <string>
+
 namespace bioinfo {
     double factorial(int n);
     unsigned long int fibonacci(unsigned int n);
     double binomialDistribution(unsigned int n, unsigned int x, double p);
+
+    class TotalPermutations {
+        private:
+            unsigned int n;
+            std::vector<std::vector<unsigned int>> p;
+        public:
+            TotalPermutations(unsigned int n);
+            std::string getPermutationSummary();
+    };
 }
 
 #endif
--- src/biomath.cpp
+++ src/biomath.cpp
@@ -3,6 +3,9 @@
 #include <limits.h>
 #include <vector>
 #include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
 
 namespace bioinfo {
     // Calculate the factorial of a number `n`.
@@ -56,5 +59,49 @@ namespace bioinfo {
         prob = ( factorial(n) / (factorial(n-x) * factorial(x)) ) * pow(p, x) * pow(q, n - x);
 
         return prob;
+    }
+
+    // Create a new `TotalPermutations` object that has all possible permuations of the positive integers with a 
+    // permutations length of `n`.
+    TotalPermutations::TotalPermutations(unsigned int n) {
+        (*this).n = n;
+        std::vector<std::vector<unsigned int>> p;
+        std::vector<unsigned int> v;
+
+        int i;
+
+        if (n > 0) {
+            for (i = 1; i < n + 1; ++i) {
+                v.push_back(i);
+            }
+
+            do {
+                p.push_back(v);
+            } while (std::next_permutation(v.begin(), v.end()));
+        }
+
+        (*this).p = p;
+    }
+
+    // Returns a string summary with the total number of permutaions on the first line with a new permutations on each of the
+    // following lines.
+    std::string TotalPermutations::getPermutationSummary() {
+        std::stringstream ss;
+        std::vector<std::vector<unsigned int>>::iterator row;
+        std::vector<unsigned int>::iterator col;
+
+        ss << factorial(n) << "\n";
+
+        for (row = (*this).p.begin(); row != (*this).p.end(); row++) {
+            for (col = row->begin(); col != row->end(); col++) {
+                ss << *col << " ";
+            }
+
+            if (row != (*this).p.end() - 1) {
+                ss << "\n";
+            }
+        }
+
+        return ss.str();
     }
 }
